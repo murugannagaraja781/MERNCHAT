82bf1668f1b01143b70e88d99638f3af0e186def
murugannagaraja781 <murugannagaraja781@gmail.com>
2025-11-30 14:16:24 +0530

    Update envVariables.js

--- client/src/configs/envVariables.js
+++ client/src/configs/envVariables.js
@@ -1,25 +1,25 @@
-const Reset = '\x1b[0m';
-const FgYellow = '\x1b[33m';
+const Reset = "\x1b[0m";
+const FgYellow = "\x1b[33m";
 
-const APP_ENV = 'prod'
-let APP_HOST = 'https://mernchat-production-d169.up.railway.app'
+const APP_ENV = "prod";
+let APP_HOST = "https://mernchat-production-d169.up.railway.app";
 
 switch (APP_ENV) {
-    case 'local':
-        console.log('connecting to local')
-        APP_HOST = 'http://localhost:5000'
-        break;
-    case 'prod':
-        console.log('connecting to prod')
-        APP_HOST = 'https://affectionate-benevolence-production-220f.up.railway.app'
-        break;
-    default:
-        console.log('connecting to default api (local)')
-        APP_HOST = 'https://affectionate-benevolence-production-220f.up.railway.app'
-        break;
+  case "local":
+    console.log("connecting to local");
+    APP_HOST = "http://localhost:5000";
+    break;
+  case "prod":
+    console.log("connecting to prod");
+    APP_HOST = "https://mernchat-production-d169.up.railway.app";
+    break;
+  default:
+    console.log("connecting to default api (local)");
+    APP_HOST = "https://mernchat-production-d169.up.railway.app";
+    break;
 }
 
-console.log(FgYellow, APP_ENV, Reset)
-console.log(FgYellow, APP_HOST, Reset)
+console.log(FgYellow, APP_ENV, Reset);
+console.log(FgYellow, APP_HOST, Reset);
 
 export default APP_HOST;
